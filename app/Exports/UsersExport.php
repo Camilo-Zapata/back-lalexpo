2e8c6987cd00d455d5614b20d47638828caf65e8
Camilo-Zapata <camilozgbmediagroup@gmail.com>
2024-03-09 09:15:28 -0500

    excel

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -4,8 +4,12 @@ namespace App\Exports;
 
 use App\Models\User;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithColumnWidths;
 
-class UsersExport implements FromCollection
+
+
+class UsersExport implements FromCollection,WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -13,5 +17,47 @@ class UsersExport implements FromCollection
     public function collection()
     {
         return User::where('sort', 1)->get();
+    }
+
+
+    public function columnWidths(): array
+    {
+        return [
+            'A' => 55,
+            'B' => 60,  
+            'C' => 60,  
+            'D' => 90,
+            'E' => 60, 
+            'F' => 80, 
+            'G' => 60,  
+            'H' => 60,
+            'I' => 60,
+            'J' => 80,
+            'K' => 60,
+            'L' => 100,
+            'M' => 60,
+            'N' => 60,
+                 
+        ];
+    }
+
+
+    public function headings(): array
+    {
+        return [
+            'Id',
+            'Nombre',
+            'Apellido',
+            'Correo',
+            'Teléfono',
+            'Pais',
+            'Ciuda',
+            'Rol',
+            'Empresa',
+            'Estado Sorteo',
+            'Instagram',
+            'Fecha de Creacion',
+            'Fecha de Actualizacion'
+        ];
     }
 }
